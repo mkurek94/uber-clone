6ea604aad15b737f62f9a2b1b4d05bd9ead87892
Mateusz Kurek <m.kurek1994@gmail.com>
2024-11-10 23:29:58 +0100

    add onboarding view

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
 export default function Home() {
-  return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text>Home</Text>
-      <StatusBar style="dark" />
-    </SafeAreaView>
-  );
+  return <Redirect href="./(auth)/welcome" />;
 }
